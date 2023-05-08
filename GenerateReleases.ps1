5a9bd964e4c3ecba6d301e7f53f3c593f579c57f
José Espinoza Bone <megamanx@outlook.com>
2023-05-08 17:02:32 -0500

    Version file + Releases script.

---
+++ GenerateReleases.ps1
@@ -0,0 +1,23 @@
+
+function Copy-Files {
+    param (
+        [Parameter()]
+        [ValidateSet("Debug", "Release")]
+        [string]
+        $Configuration,
+        
+        [Parameter()]
+        [ValidateSet("x64")]
+        [string]
+        $Platform
+        )
+        $folder = "Releases"
+        xcopy "$($PSScriptRoot)\$Platform\$Configuration\*.dll" "$($PSScriptRoot)\$folder\$Platform\$Configuration\bin\" /Y
+        xcopy "$($PSScriptRoot)\$Platform\$Configuration\Insane.pdb" "$($PSScriptRoot)\$folder\$Platform\$Configuration\bin\" /Y
+        xcopy "$($PSScriptRoot)\$Platform\$Configuration\Insane.exp" "$($PSScriptRoot)\$folder\$Platform\$Configuration\bin\" /Y
+        xcopy "$($PSScriptRoot)\$Platform\$Configuration\Insane.lib" "$($PSScriptRoot)\$folder\$Platform\$Configuration\lib\" /Y
+    }
+$ErrorActionPreference = 'Stop' 
+& "$PSScriptRoot/RebuildAll.ps1"
+Copy-Files -Configuration Debug -Platform x64
+Copy-Files -Configuration Release -Platform x64
--- RebuildAll.ps1
+++ RebuildAll.ps1
@@ -1,8 +1,19 @@
-﻿Write-Host "███ Debug - Build"
-msbuild InsaneWindows.sln /t:Insane:Rebuild /p:Platform=x64 /p:Configuration=Debug /p:BuildProjectReferences=true /m
-msbuild InsaneWindows.sln /t:Insane_Console_Import:Rebuild /p:Platform=x64 /p:Configuration=Debug /p:BuildProjectReferences=true /m
-msbuild InsaneWindows.sln /t:Insane_Console_Play:Rebuild /p:Platform=x64 /p:Configuration=Debug /p:BuildProjectReferences=true /m
-Write-Host "███ Release - Build"
-msbuild InsaneWindows.sln /t:Insane:Rebuild /p:Platform=x64 /p:Configuration=Release /p:BuildProjectReferences=true /m
-msbuild InsaneWindows.sln /t:Insane_Console_Import:Rebuild /p:Platform=x64 /p:Configuration=Release /p:BuildProjectReferences=true /m
-msbuild InsaneWindows.sln /t:Insane_Console_Play:Rebuild /p:Platform=x64 /p:Configuration=Release /p:BuildProjectReferences=true /m
+﻿function Build-AllProjects {
+    param (
+        [Parameter()]
+        [ValidateSet("Debug", "Release")]
+        [string]
+        $Configuration,
+        
+        [Parameter()]
+        [ValidateSet("x64")]
+        [string]
+        $Platform
+        )
+        msbuild InsaneWindows.sln /t:Insane:Rebuild /p:Platform=$Platform /p:Configuration=$Configuration /p:BuildProjectReferences=true /m
+        msbuild InsaneWindows.sln /t:Insane_Console_Import:Rebuild /p:Platform=$Platform /p:Configuration=$Configuration /p:BuildProjectReferences=true /m
+        msbuild InsaneWindows.sln /t:Insane_Console_Play:Rebuild /p:Platform=$Platform /p:Configuration=$Configuration /p:BuildProjectReferences=true /m
+    }
+$ErrorActionPreference = 'Stop'
+Build-AllProjects -Configuration Debug -Platform x64
+Build-AllProjects -Configuration Release -Platform x64
